0f2e4467ed47cea117e937f785014c8d3e6e802b
cheonga <cheonga@naver.com>
2021-07-23 14:26:08 +0900

    cheonga

--- src/main/java/kh/spring/controller/BoardController.java
+++ src/main/java/kh/spring/controller/BoardController.java
@@ -1,12 +1,15 @@
 package kh.spring.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import kh.spring.dao.MemberDAO;
+import kh.spring.dao.BoardDAO;
+import kh.spring.dto.BoardDTO;
 
 @Controller
 @RequestMapping("/bod")
@@ -17,8 +20,11 @@ public class BoardController {
 	
 	@Autowired
 	private HttpSession session;
-
 	
-
+	@RequestMapping("/boardlist")
+	public List<BoardDTO> boardlist(int cpage) {
+		System.out.println("요청페이지 : " + cpage);
+		List<BoardDTO> list = dao.boardlist(cpage);
+	}
 
 }
