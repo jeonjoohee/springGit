ee071616761217cbce8d5a270211efab4655fbcb
jh <jh@naver.com>
2021-07-23 17:02:08 +0900

    import

--- src/main/java/kh/spring/controller/BoardController.java
+++ src/main/java/kh/spring/controller/BoardController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import kh.spring.config.BoardConfig;
 import kh.spring.dao.BoardDAO;
 import kh.spring.dto.BoardDTO;
 
